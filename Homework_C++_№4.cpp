acaae1e89a3de791bb6454cd3d9e51051ca73f11
Antokl <liantokha@mail.ru>
2024-08-27 15:09:32 +0300

    Add project files.

---
+++ Homework_C++_№4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+void Print_num(int N, int k)
+{
+    for (int i = k; i <= N; i += 2)
+        cout << i << " ";
+    cout << "\n";
+}
+
+int main()
+{
+    int N;
+    cout << "N="; cin >> N;
+
+    cout << "Even:\n";
+    Print_num(N, 0);
+
+    cout << "Odd:\n";
+    Print_num(N, 1);
+
+    system("pause");
+    return 0;
+}
